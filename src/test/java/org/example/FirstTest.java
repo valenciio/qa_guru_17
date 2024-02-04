24d4c41a3e6195219a299cbcce8ccfcd80b996f2
Lenovo Gaming <selezenevva@krotech.ru>
2024-02-04 14:05:58 +0300

    first commit

---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void firstTest () {
+        Assertions.assertTrue(3 > 2);
+    }
+}
